35ff25b355b8a66e9fbd61807627a4b436a5aac7
Saurabh Negi <saurabhnegi.snn@gmail.com>
2025-01-07 21:39:28 +0530

    fix isLoading

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -3,7 +3,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-    isLoading: false,
+    isLoading: true,
     isAuthenticated: false,
     user: null
 }
