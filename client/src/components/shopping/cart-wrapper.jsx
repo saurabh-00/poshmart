1161a2db19725569cc97063794c964302fc55f31
Saurabh Negi <saurabhnegi.snn@gmail.com>
2025-01-20 12:43:31 +0530

    checkout btn fix

--- client/src/components/shopping/cart-wrapper.jsx
+++ client/src/components/shopping/cart-wrapper.jsx
@@ -25,26 +25,29 @@ const UserCartWrapper = ({ cartItems, setOpenCartSheet }) => {
       </SheetHeader>
       <div className="mt-8 space-y-4">
         {cartItems && cartItems.length > 0 ? (
-          cartItems.map((item, i) => (
-            <Fragment key={item?.productId || `${item?.title}_${i}`}>
-              <UserCartItemsContent cartItem={item} />
-              <div className="mt-8 space-y-4">
-                <div className="flex justify-between">
-                  <span className="font-bold">Total</span>
-                  <span className="font-bold">${totalCartAmount}</span>
-                </div>
+          <Fragment>
+            {cartItems.map((item, i) => (
+              <UserCartItemsContent
+                key={item?.productId || `${item?.title}_${i}`}
+                cartItem={item}
+              />
+            ))}
+            <div className="mt-8 space-y-4">
+              <div className="flex justify-between">
+                <span className="font-bold">Total</span>
+                <span className="font-bold">${totalCartAmount}</span>
               </div>
-              <Button
-                onClick={() => {
-                  navigate("/shop/checkout");
-                  setOpenCartSheet(false);
-                }}
-                className="w-full mt-6"
-              >
-                Checkout
-              </Button>
-            </Fragment>
-          ))
+            </div>
+            <Button
+              onClick={() => {
+                navigate("/shop/checkout");
+                setOpenCartSheet(false);
+              }}
+              className="w-full mt-6"
+            >
+              Checkout
+            </Button>
+          </Fragment>
         ) : (
           <h3 className="font-semibold">Your cart is empty!</h3>
         )}
