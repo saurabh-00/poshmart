421f47b0c2631c5e35ce430781a5a980e788d0cb
Saurabh Negi <saurabhnegi.snn@gmail.com>
2025-01-22 20:27:05 +0530

    common slice

---
+++ client/src/store/common-slice/index.js
@@ -0,0 +1,63 @@
+import { apiUrl } from "@/config";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+    isLoading: false,
+    featureImageList: []
+};
+
+export const getFeatureImages = createAsyncThunk(
+    "common/getFeatureImages",
+    async () => {
+        const response = await axios.get(
+            `${apiUrl}/common/features`,
+            { withCredentials: true }
+        );
+        return response.data;
+    }
+);
+
+export const addFeatureImage = createAsyncThunk(
+    "common/addFeatureImage",
+    async (image) => {
+        const response = await axios.post(
+            `${apiUrl}/common/features`,
+            { image }
+        );
+        return response.data;
+    }
+);
+
+const commonSlice = createSlice({
+    name: "common",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(getFeatureImages.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(getFeatureImages.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.featureImageList = action.payload.data;
+            })
+            .addCase(getFeatureImages.rejected, (state) => {
+                state.isLoading = false;
+                state.featureImageList = [];
+            })
+            .addCase(addFeatureImage.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(addFeatureImage.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.featureImageList = [action.payload.data, ...state.featureImageList];
+            })
+            .addCase(addFeatureImage.rejected, (state) => {
+                state.isLoading = false;
+                state.featureImageList = [];
+            });
+    },
+});
+
+export default commonSlice.reducer;
