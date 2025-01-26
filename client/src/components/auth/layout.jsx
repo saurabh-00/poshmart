a59789b58ab9075d869318ebb85ef27cd5ed81e1
Saurabh Negi <saurabhnegi.snn@gmail.com>
2025-01-26 13:18:19 +0530

    added typewriter effect

--- client/src/components/auth/layout.jsx
+++ client/src/components/auth/layout.jsx
@@ -1,12 +1,26 @@
 import { Outlet } from "react-router-dom";
+import Typewriter from "typewriter-effect";
+import logo from "../../assets/vite.svg";
+
+const message = "Welcome to PoshMart!";
 
 const AuthLayout = () => {
   return (
     <div className="flex min-h-screen w-full">
       <div className="hidden lg:flex items-center justify-center bg-black w-1/2 px-12">
-        <div className="max-w-md space-y-6 text-center text-primary-foreground">
+        <div className="max-w-md space-y-6 text-center text-primary-foreground flex flex-col items-center">
+          <img src={logo} alt="logo" className="w-10 h-10" />
           <h1 className="text-4xl font-extrabold tracking-tight">
-            Welcome to PoshMart!
+            <Typewriter
+              options={{ delay: 100 }}
+              onInit={(typewriter) => {
+                typewriter
+                  .changeDelay(100)
+                  .pauseFor(500)
+                  .typeString(message)
+                  .start();
+              }}
+            />
           </h1>
         </div>
       </div>
